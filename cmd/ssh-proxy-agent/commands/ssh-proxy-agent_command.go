4db5825864fde430b6bab34a109d50dd97a1fa5f
Ryan Norton <rnorton@instructure.com>
2020-01-20 17:05:21 -0700

    tweak command usage to be closer to ssh-proxy
    in an effort to move usage closer to the existing ssh-proxy this
    pares the subcommands out to consolidate on the root command

--- cmd/ssh-proxy-agent/commands/ssh-proxy-agent_command.go
+++ cmd/ssh-proxy-agent/commands/ssh-proxy-agent_command.go
@@ -1,7 +1,11 @@
 package commands
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
+
+	"github.com/miquella/ssh-proxy-agent/operations"
 )
 
 // SSHProxyAgentCommand is the root command for the `ssh-proxy-agent` entrypoint
@@ -9,9 +13,37 @@ var SSHProxyAgentCommand = &cobra.Command{
 	Use:   "ssh-proxy-agent",
 	Short: "SSH-Proxy-Agent creates an ssh-agent proxy",
 
+	RunE:         shellRunE,
 	SilenceUsage: true,
 }
 
+var interactive bool
+var shell = operations.Spawn{}
+var username string
+
 func init() {
-	SSHProxyAgentCommand.AddCommand(ShellCommand)
+	SSHProxyAgentCommand.Flags().BoolVarP(&interactive, "", "l", false, "spawn an interactive shell")
+
+	SSHProxyAgentCommand.Flags().BoolVar(&shell.GenerateKey, "generate-key", false, "generate RSA key pair (default: false)")
+	SSHProxyAgentCommand.Flags().StringVarP(&username, "username", "u", os.Getenv("USER"), "username for key signing")
+	SSHProxyAgentCommand.Flags().StringVar(&shell.VaultSigningUrl, "vault-signing-url", "", "HashiCorp Vault url to sign SSH keys")
+}
+
+func shellRunE(cmd *cobra.Command, args []string) error {
+	if interactive {
+		shell.Command = loginShellCommand()
+		shell.Username = username
+		return shell.Run()
+	} else {
+		return cmd.Usage()
+	}
+}
+
+func loginShellCommand() []string {
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "/bin/sh"
+	}
+
+	return []string{shell, "--login"}
 }
